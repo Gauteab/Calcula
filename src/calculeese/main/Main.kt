54bc7e5379e1ca519879288af5cd5dffdce57cd4
gaute <gaute.berge@hotmail.com>
2019-04-30 16:01:26 +0200

    log function (improved enter parser)

--- src/calculeese/main/Main.kt
+++ src/calculeese/main/Main.kt
@@ -1,14 +1,17 @@
 package calculeese.main
 
+import calculeese.main.parser.parseTerm
 import calculeese.main.scanner.Scanner
 import calculeese.main.scanner.Token
 
 fun main() {
     val s = Scanner("calc/mini.cal")
-    while (true) {
-        println("Peek: ${s.curToken()}")
-        val t = s.nextToken()
-        println(t)
-        if (t == Token.Eof) break
-    }
+    //while (true) {
+    //    println("Peek: ${s.curToken()}")
+    //    val t = s.nextToken()
+    //    println(t)
+    //    if (t == Token.Eof) break
+    //}
+
+    println(parseTerm(s))
 }
--- src/calculeese/main/parser/Expr.kt
+++ src/calculeese/main/parser/Expr.kt
@@ -22,13 +22,8 @@ sealed class Expr {
     }
 }
 
-fun parseError(msg: String): Nothing {
-    println("Parse Error: $msg")
-    exitProcess(-1)
-}
-
-fun parseTerm(s: Scanner): Term {
-    TODO()
+fun parseTerm(s: Scanner): Term = log {
+    Term(listOf(), listOf())
 }
 
 fun parseTermOpr(s: Scanner): TermOpr {
@@ -52,3 +47,18 @@ fun parseAtom(s: Scanner): Atom = when (val t = s.curToken()) {
 fun parseInnerExpr(s: Scanner): IntExpr {
     TODO()
 }
+
+
+var indent = 0
+inline fun <reified T> log(name: String = T::class.simpleName!!, block: () -> T): T {
+    println("${"  ".repeat(indent++)}<$name>")
+    val t = block()
+    println("${"  ".repeat(--indent)}</$name>")
+    return t
+}
+
+fun parseError(msg: String): Nothing {
+    println("Parse Error: $msg")
+    exitProcess(-1)
+}
+
